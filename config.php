bdb7e373c92eadb7ea10370ee08e8e632afce819
leo2695 <leoaguilar26@hotmail.com>
2019-07-17 18:21:05 -0600

    Initial commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php //este archivo es donde se van a guardar las llaves
+
+
+require_once ('paypalsdk/autoload.php');
+
+define('URL_SITIO','http://localhost/PagosPaypal/paypal');
+
+//instalar el API de paypal
+$apiContext=new \PayPal\Rest\ApiContext( //$apiContext se puede llamar como yo quiera pero asi es lo normal
+new \PayPal\Auth\OAuthTokenCredential( //instanciando
+//Cliente ID
+'AcLKxeka0w5xgmile3yhqF39Au-V1PXey0An6stJSezItQZIKYy0Ki-aKqvlV9jSCotzkI4HtwLgEmJa',
+//Secret
+'EOAoSIHlxaeic9n6tHjWNU0LpndpGS0e-0mppuIvGRTsdafRMvSbZeSxLXhc8zN9lC2Xgc7SXTPVTgoF'
+)
+);
+
+//var_dump($apiContext);
+?>
---
+++ pagar.php
@@ -0,0 +1,95 @@
+<?php
+
+ini_set("display_errors", "1"); //para ver errores
+error_reporting(E_ALL);
+
+if(!isset($_POST['producto'], $_POST['precio'])){
+    exit("Hubo un error");
+}
+//en php se puede tener muchas clases con ese nombre solo que con ruta distinta con los namespace se puede especificar
+use PayPal\Api\Payer; //namespace
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Details; //namespace
+use PayPal\Api\Amount; //namespace
+use PayPal\Api\Transaction; //namespace
+use PayPal\Api\RedirectUrls; //namespace
+use PayPal\Api\Payment; //namespace
+require_once ('config.php');
+
+$producto= htmlspecialchars($_POST['producto']);// htmlspecialchars para agregarle sanitizacion
+$precio=htmlspecialchars($_POST['precio']);
+$precio=(int) $precio; //float para que acepte decimales
+$envio=0;
+$total=$precio+$envio;
+
+/*echo "<pre>";
+var_dump($_POST);
+echo "</pre>";*/
+
+
+//Payer
+$compra=new Payer(); //enbloba todo lo del pago
+$compra->setPaymentMethod('paypal'); //en Payer esta esa funcion
+
+//echo $compra->getPaymentMethod();
+
+$articulo=new Item(); //engloba lo del articulo
+$articulo->setName($producto)
+         ->setCurrency('USD')
+         ->setQuantity(1)
+         ->setPrice($precio);
+
+
+//echo $articulo->getPrice();
+
+//Itemlist 
+$listaArticulos=new ItemList();//se pasa todos los articulos que se van a cobrar
+$listaArticulos->setItems(array($articulo));
+
+//Details
+$detalles=new Details();
+$detalles->setShipping($envio)
+         ->setSubtotal($precio);
+
+/*  */
+$cantidad=new Amount();
+$cantidad->setCurrency('USD')
+        ->setTotal($precio)
+        ->setDetails($detalles);
+
+/*   */
+$transaccion=new Transaction();
+$transaccion->setAmount($cantidad)
+->setItemList($listaArticulos)
+->setDescription('Pago')
+->setInvoiceNumber(uniqid());
+
+//echo $transaccion->getInvoiceNumber();
+
+/*  */
+$redireccionar=new RedirectUrls();
+$redireccionar->setReturnUrl(URL_SITIO."pago-finalizado.php?exito=true")
+->setCancelUrl(URL_SITIO."pago-finalizado.php?exito=false");
+
+//echo $redireccionar->getReturnUrl();
+
+/*  */
+$pago=new Payment();
+$pago->setIntent("sale")
+->setPayer($compra)
+->setRedirectUrls($redireccionar)
+->setTransactions(array($transaccion));
+try {
+    $pago->create($apiContext);
+} catch (PayPal\Exception\PayPalConnectionException $pce) {
+    echo "<pre>";
+    print_r(json_encode($pce->getData()));
+    exit;//se encarga de que el programa no se ejecute
+    echo "</pre>";
+}
+
+$aprobado=$pago->getApprovalLink();
+header("Location: {$aprobado}"); //para redireccionar
+
+?>
